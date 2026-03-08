d14d5817e63d08431afdc520eea42f0a1b944e35
Aman <40954160+thesparkvision@users.noreply.github.com>
2026-03-08 21:51:33 +0000

    debug: using different log level for debugging pipeline

--- read_every_week/main.py
+++ read_every_week/main.py
@@ -26,11 +26,15 @@ def cli() -> None:
     pipeline.run(dry_run=not args.write, retry_errors=args.retry_errors)
 
 def run():
-    log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
+    # log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
 
+    # logging.basicConfig(
+    #     level=getattr(logging, log_level, logging.WARNING),
+    #     format="%(asctime)s %(levelname)s %(message)s",
+    # )
     logging.basicConfig(
-        level=getattr(logging, log_level, logging.WARNING),
-        format="%(asctime)s %(levelname)s %(message)s",
+        level=logging.INFO,
+        format="%(asctime)s %(levelname)s %(message)s\n",
     )
     is_dry_run = os.getenv("DRY_RUN", "true").lower() == "true"
     retry_errors = os.getenv("RETRY_ERRORS", "false").lower() == "true"
